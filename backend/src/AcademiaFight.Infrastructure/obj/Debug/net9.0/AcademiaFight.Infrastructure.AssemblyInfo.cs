6e0ba530682189428dbe78661410ff7f830a6264
Gabriel Forny <gfmtech21@gmail.com>
2026-06-13 11:58:12 -0300

    ajustes

--- backend/src/AcademiaFight.Infrastructure/obj/Debug/net9.0/AcademiaFight.Infrastructure.AssemblyInfo.cs
+++ backend/src/AcademiaFight.Infrastructure/obj/Debug/net9.0/AcademiaFight.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademiaFight.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff57dfe21222b254f16731bcc6d6c8d23975cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeadbac2ec7532396237c79cb2c95bcc5b0e99b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademiaFight.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademiaFight.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
