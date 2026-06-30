a01af4c6e040dca1f55f7661551e70105b65f8db
Gabriel Forny <gfmtech21@gmail.com>
2026-06-30 17:02:37 -0300

    ajustes

--- backend/src/AcademiaFight.Domain/obj/Debug/net9.0/AcademiaFight.Domain.AssemblyInfo.cs
+++ backend/src/AcademiaFight.Domain/obj/Debug/net9.0/AcademiaFight.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademiaFight.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2d3930738a3c03aeab45b5f5d52620c7ad2f91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da87127b70b869bb961000ad1666d007e3b7efa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademiaFight.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademiaFight.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
